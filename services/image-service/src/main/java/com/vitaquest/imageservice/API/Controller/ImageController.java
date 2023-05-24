ebbe955894c51375ba44def1ec4f2066a96f92f9
Gijs Rongen <rongen.gijs@gmail.com>
2023-05-24 13:46:45 +0200

    Code cleanup

--- services/image-service/src/main/java/com/vitaquest/imageservice/API/Controller/ImageController.java
+++ services/image-service/src/main/java/com/vitaquest/imageservice/API/Controller/ImageController.java
@@ -1,13 +1,10 @@
 package com.vitaquest.imageservice.API.Controller;
 
 import com.nimbusds.jose.shaded.json.JSONArray;
-import com.vitaquest.imageservice.Domain.DTO.AddImageDTO;
-import com.vitaquest.imageservice.Domain.Models.Image;
 import com.vitaquest.imageservice.Domain.Service.ImageService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.reflect.FieldUtils;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +14,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.util.Base64;
 import java.util.Map;
 
 @Api(tags = "Image Controller")
@@ -36,57 +29,14 @@ public class ImageController {
     }
     @ApiOperation(value = "Add image")
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public @ResponseBody ResponseEntity<Image> addImage(@RequestParam("image") MultipartFile image) throws Exception {
+    public @ResponseBody ResponseEntity<String> addImage(@RequestParam("image") MultipartFile image) throws Exception {
         Authentication authContext = SecurityContextHolder.getContext().getAuthentication();
         if (isAdmin(authContext)) {
-            System.out.println("add image:" + image);
-            AddImageDTO dto = new AddImageDTO();
-            dto.setData(Base64.getEncoder().encodeToString(image.getBytes()).getBytes());
-            dto.setName(image.getOriginalFilename());
-            dto.setContentType(image.getContentType());
-            dto.setSize(image.getSize());
-            //dto.setContentType(contentType);
-            System.out.println("ImageDTO Name: " + dto.getName());
-            System.out.println("ImageDTO getContentType: " + dto.getContentType());
-            System.out.println("ImageDTO getSize: " + dto.getSize());
-            //System.out.println("ImageDTO getData: " + Arrays.toString(dto.getData()));
-            System.out.println("getOriginalFilename name: " + image.getOriginalFilename());
-            return new ResponseEntity<>(service.addImage(dto), HttpStatus.CREATED);
+            return new ResponseEntity<>(service.addImage(image.getBytes()), HttpStatus.CREATED);
         } else {
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
         }
     }
-
-    @ApiOperation(value = "Get image by ID")
-    @GetMapping(value = "/{imageId}", produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
-    public ResponseEntity<byte[]> getImageByID(@PathVariable String imageId) throws Exception {
-        System.out.println("get image by id: " + imageId);
-        BufferedImage image = service.getImageByID(imageId);
-
-        System.out.println("image: " + image);
-        if (image == null) {
-            System.out.println("image is null");
-            return ResponseEntity.notFound().build();
-        }
-
-        MediaType contentType;
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
-        if (image.getType() == BufferedImage.TYPE_3BYTE_BGR || image.getType() == BufferedImage.TYPE_4BYTE_ABGR) {
-            contentType = MediaType.IMAGE_PNG;
-            ImageIO.write(image, "png", outputStream);
-        } else {
-            contentType = MediaType.IMAGE_JPEG;
-            ImageIO.write(image, "jpeg", outputStream);
-        }
-        byte[] imageBytes = outputStream.toByteArray();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(contentType);
-        headers.setContentLength(imageBytes.length);
-
-        return ResponseEntity.ok().headers(headers).body(imageBytes);
-    }
-
 
     @ApiOperation(value = "Delete image by ID")
     @DeleteMapping(value = "/{imageId}")
--- services/image-service/src/main/java/com/vitaquest/imageservice/Domain/Service/ImageService.java
+++ services/image-service/src/main/java/com/vitaquest/imageservice/Domain/Service/ImageService.java
@@ -4,24 +4,14 @@ import com.microsoft.azure.storage.CloudStorageAccount;
 import com.microsoft.azure.storage.blob.CloudBlobClient;
 import com.microsoft.azure.storage.blob.CloudBlobContainer;
 import com.microsoft.azure.storage.blob.CloudBlockBlob;
-import com.vitaquest.imageservice.Domain.DTO.AddImageDTO;
-import com.vitaquest.imageservice.Domain.Models.Image;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.util.Base64;
 import java.util.UUID;
 
 @Service
 public class ImageService {
-
-    @Value("${AZURE_STORAGE_ACCOUNT_ENDPOINT}")
-    private String AZURE_STORAGE_ACCOUNT_ENDPOINT;
-
     @Value("${AZURE_STORAGE_ACCOUNT_KEY}")
     private String accountKey;
 
@@ -31,40 +21,7 @@ public class ImageService {
     @Value("${AZURE_STORAGE_ACCOUNT_NAME}")
     private String accountName;
 
-    public BufferedImage getImageByID(String blobName) throws Exception {
-        // Create a connection string to your Azure Storage account
-        String connectionString = String.format("DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s;", accountName, accountKey);
-
-        // Create a CloudStorageAccount object from the connection string
-        CloudStorageAccount storageAccount = CloudStorageAccount.parse(connectionString);
-
-        // Create a CloudBlobClient object from the CloudStorageAccount object
-        CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
-
-        // Get a reference to the container
-        CloudBlobContainer container = blobClient.getContainerReference(containerName);
-
-        // Get a reference to the blob
-        CloudBlockBlob blob = container.getBlockBlobReference(blobName);
-
-        System.out.println(String.format("Downloading blob '%s' from container '%s' in storage account '%s'...", blobName, containerName, accountName));
-
-        // Download the blob to a byte array
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        blob.download(outputStream);
-        byte[] imageBytes = outputStream.toByteArray();
-
-        System.out.println(String.format("Blob '%s' downloaded successfully from container '%s' in storage account '%s'.", blobName, containerName, accountName));
-
-        // Convert the byte array to a BufferedImage object
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(imageBytes);
-        BufferedImage bufferedImage = ImageIO.read(inputStream);
-
-        return bufferedImage;
-    }
-
-
-    public Image addImage(AddImageDTO dto) throws Exception {
+    public String addImage(byte[] imageBytes) throws Exception {
         // Create a connection string to your Azure Storage account
         String connectionString = String.format("DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s;", accountName, accountKey);
 
@@ -82,28 +39,18 @@ public class ImageService {
         // Create a CloudBlockBlob object and set the blob name
         CloudBlockBlob blob = container.getBlockBlobReference(generatedName);
 
-        byte[] decodedImageData = Base64.getDecoder().decode(dto.getData());
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(decodedImageData);
-        blob.upload(inputStream, decodedImageData.length);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(imageBytes);
+        blob.upload(inputStream, imageBytes.length);
 
-        System.out.println(String.format("Blob '%s' uploaded successfully to container '%s' in storage account '%s'.", generatedName, containerName, accountName));
+        System.out.printf("Blob '%s' uploaded successfully to container '%s' in storage account '%s'.%n", generatedName, containerName, accountName);
 
         blob.uploadProperties();
 
-        // Create an Image object and set the properties
-        Image newImage = new Image();
-        newImage.setId(generatedName);
-        newImage.setUrl(blob.getUri().toString());
-        newImage.setSize(dto.getSize());
-
-        return newImage;
+        //return the image url
+        return blob.getUri().toString();
     }
-
-
 
     public void deleteImageByID(String imageId) throws Exception {
 
     }
-
-
 }
